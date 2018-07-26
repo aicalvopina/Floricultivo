e35dacb66ab2135fc9b226754632c0f1503dc2ba
aicalvopina <kyubi900@hotmail.com>
2018-07-26 18:51:53 -0500

    creacion clases temperatura hora

--- Capa de control/GradoDia.cs
+++ Capa de control/GradoDia.cs
@@ -8,5 +8,17 @@ namespace Floricultivo.Capa_de_control
 {
     class GradoDia
     {
+        private double temperaturaMaxima;
+        private double temperaturaMinima;
+        private double temperaturaBase;
+        private double gradosDia;
+        private double gradosHora;
+
+        private double gradosDiaCalculo()
+        {
+            this.gradosDia = ((this.temperaturaMaxima+this.temperaturaMinima)/2)-this.temperaturaBase;
+            return this.gradosDia;
+        }
+
     }
 }
---
+++ Capa de control/TemperaturaHora.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Floricultivo.Capa_de_DB;
+
+namespace Floricultivo.Capa_de_control
+{
+    class TemperaturaHora
+    {
+
+    }
+}
