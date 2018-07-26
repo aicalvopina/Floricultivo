c100d772bf18adfb305240ed5368a6cd7d81e4de
jarm14 <joelram1@hotmail.com>
2018-07-25 22:11:20 -0500

    Conexion BD
    -Objeto de conexión a base de datos creado
    -Objetos Dia y Temperatura creados
    -Servicio para temperatura creado

--- Capa de DB/ConexionDB.cs
+++ Capa de DB/ConexionDB.cs
@@ -3,10 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace Floricultivo.Capa_de_DB
 {
-    class ConexionDB
+    public class ConexionDB
     {
+        #region Datos miembro
+        private MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+        private MySqlConnection conexion;
+        #endregion
+
+        #region
+        public MySqlConnection Conexion
+        {
+            get { return conexion; }
+            set { conexion = value; }
+        }
+        #endregion
+
+        #region Funciones
+        public void crearConexion() 
+        {
+            try
+            {
+                this.builder.Server = "192.168.110.14";
+                this.builder.UserID = "floricultivo";
+                this.builder.Password = "Floricultivo112358.";
+                this.builder.Database = "floricultivo";
+                this.conexion = new MySqlConnection(builder.ToString());
+                this.conexion.Open();
+            }
+            catch (Exception ex) 
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void cerrarConexion() 
+        {
+            try
+            {
+                this.conexion.Close();
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+        }
+        #endregion
     }
 }
---
+++ Capa de DB/Temperatura.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Floricultivo.Capa_de_DB
+{
+    public class Temperatura
+    {
+        #region Datos miembro
+        private Int32 codTemperatura;
+        private Int32 codDia;
+        private DateTime hora;
+        private double temperaturaGrados;
+        #endregion
+
+        #region Constructor
+        public Temperatura() 
+        {
+            this.codTemperatura = 0;
+            this.codDia = 0;
+            this.hora = DateTime.Now;
+            this.temperaturaGrados = 0.0f;
+        }
+        #endregion
+
+        #region Getter y Setters
+        public int CodTemperatura
+        {
+            get { return codTemperatura; }
+            set { codTemperatura = value; }
+        }
+        public int CodDia
+        {
+            get { return codDia; }
+            set { codDia = value; }
+        }
+
+        public DateTime Hora
+        {
+            get { return hora; }
+            set { hora = value; }
+        }
+        public double TemperaturaGrados
+        {
+            get { return temperaturaGrados; }
+            set { temperaturaGrados = value; }
+        }
+        #endregion
+    }
+}
