f3e172109c3f77691bb2a22d00d15e037fea24e1
aicalvopina <kyubi900@hotmail.com>
2018-07-27 01:30:26 -0500

    metodo para el calculo de la temperatura-hora

--- Capa de control/GradoDia.cs
+++ Capa de control/GradoDia.cs
@@ -3,21 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Floricultivo.Capa_de_DB;
 
 namespace Floricultivo.Capa_de_control
 {
     class GradoDia
     {
+        //Parametros para el calculo
         private double temperaturaMaxima;
         private double temperaturaMinima;
         private double temperaturaBase;
+        private Temperatura tempHora;
+        //Resultados
         private double gradosDia;
         private double gradosHora;
+
 
         private double gradosDiaCalculo()
         {
             this.gradosDia = ((this.temperaturaMaxima+this.temperaturaMinima)/2)-this.temperaturaBase;
             return this.gradosDia;
+        }
+
+        private double gradosHoraCalculo()
+        {
+            this.gradosHora = tempHora.TemperaturaGrados - this.temperaturaBase;
+            return this.gradosHora;
         }
 
     }
--- Capa de control/TemperaturaHora.cs
+++ Capa de control/TemperaturaHora.cs
@@ -9,6 +9,60 @@ namespace Floricultivo.Capa_de_control
 {
     class TemperaturaHora
     {
+        private Dia diaEstimar;
+        private DiaServicio service =  new DiaServicio();
+        private TemperaturaServicio serviceTemp = new TemperaturaServicio();
+        //Parametros para la estimacion
+        private double c = 0.39;
+        private double pi = Math.PI;
+        private double difTxTn;
+        private double difTxTo;
+        private double difTpTo;
+        public TemperaturaHora(DateTime dia)
+        {
+            diaEstimar = service.obtenerPorFecha(dia);
+            diaEstimar.TemperaturaAmanecer = diaEstimar.TemperaturaMaxima - (c * (diaEstimar.TemperaturaMaxima - diaEstimar.TemperaturaSigDia));
+            difTxTo = diaEstimar.TemperaturaMaxima - diaEstimar.TemperaturaOcaso;
+            difTxTn = diaEstimar.TemperaturaMaxima - diaEstimar.TemperaturaAmanecer;
+            difTpTo = (diaEstimar.TemperaturaSigDia - diaEstimar.TemperaturaAmanecer) / Math.Sqrt(Convert.ToDouble(diaEstimar.HoraSigDia) - Convert.ToDouble(diaEstimar.HoraAmanecer));   
+        }
+        private double primerSegmento(DateTime hora)
+        {
+            return diaEstimar.TemperaturaAmanecer +
+                      (difTxTn * ((Convert.ToDouble(hora) - Convert.ToDouble(diaEstimar.HoraAmanecer)) / (Convert.ToDouble(diaEstimar.HoraMaxTemperatura) - Convert.ToDouble(diaEstimar.HoraAmanecer)) * (pi / 2)));
+        }
+        private double segundoSegmento(DateTime hora)
+        {
+            return diaEstimar.TemperaturaOcaso + (difTxTo * Math.Sin((pi / 2) + ((Convert.ToDouble(hora) - Convert.ToDouble(diaEstimar.HoraMaxTemperatura)) / 4) * (pi / 2)));
+        }
+        private double tercerSegmento(DateTime hora)
+        {
+            return diaEstimar.TemperaturaOcaso + (difTpTo * Math.Sqrt(Convert.ToDouble(hora) - Convert.ToDouble(diaEstimar.HoraOcaso)));
+        }
+        public double estimarTemperaturas(DateTime hora){
+            if (diaEstimar.HoraAmanecer < hora && hora <= diaEstimar.HoraMaxTemperatura){
+                return primerSegmento(hora);
+            }else if(diaEstimar.HoraMaxTemperatura < hora && diaEstimar.HoraOcaso > hora){
+                return segundoSegmento(hora);
+            } else if(diaEstimar.HoraOcaso < hora && hora <= diaEstimar.HoraSigDia){
+                return tercerSegmento(hora);
+            }
+            return 0;
+        }
+
+        public void guardarTemperaturas(DateTime hora)
+        {
+            DateTime horaAux = DateTime.Now;
+            Temperatura objTemp =  new Temperatura();
+            objTemp.CodDia = diaEstimar.CodDia;
+            for (int i = 0; i < 24; i++)
+            {
+                objTemp.Hora = new DateTime(horaAux.Year, horaAux.Month, horaAux.Day, i, 0, 0);
+                objTemp.TemperaturaGrados = estimarTemperaturas(objTemp.Hora);
+            }
+            serviceTemp.crearTemperatura(objTemp);
+        }
+       
 
     }
 }
